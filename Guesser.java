910e9fcabe2fc732628f410c3988f7bab4ebc12c
Hang Phan <hangphan0508@gmail.com>
2025-10-18 01:54:18 +0200

    Complete the Guesser program.

--- Guesser.java
+++ Guesser.java
@@ -12,14 +12,13 @@ public class Guesser {
         rules();
         doGuesses();
   }
-
-  private void rules() {
-    System.out.println("Think of a number between " +
-                       low + " and "  + high);
-    System.out.println("I'm going to ask a few questions in order " +
-                       "to guess the number.");
-    System.out.println("Please answer T for true, and F for false.\n");
-  }
+ private void rules() {
+        System.out.println("Think of a number between " +
+                low + " and " + high);
+        System.out.println("I'm going to ask a few questions in order " +
+                "to guess the number.");
+        System.out.println("Please answer T for true, and F for false.\n");
+    }
 
   private String getReply() {
         Console console = System.console();
@@ -49,6 +48,28 @@ public class Guesser {
         }
     }
   private void doGuesses() {
+        int i = 0;
+        int middle = 0;
+        while (low < high) {
+            middle = low + (high - low) / 2;
 
+            System.out.println("Is the number less than or equal to " +
+                    middle + "?");
+            String reply = getReply();
+            if ("T".equals(reply)) {
+                high = middle;
+            } else {
+                low = middle + 1;
+            }
+            i++;
+        }
+        answer(low, i);
+    }
+ private void answer(int guess, int numberOfGuesses){
+    System.out.println("You were thinking about " +
+                       guess +
+                       " (took me " + numberOfGuesses +
+                       " guesses)");
   }
-}
+
+ }
